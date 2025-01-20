a1e4d1f2354cd6934d9b6271e0a05e3ddf948057
Lucas Josino <contact@lucasjosino.com>
2025-01-20 13:41:01 -0300

    refactor: atualiza as mensagens do script `clean.sh`

--- projetos/projeto-linux/clean.sh
+++ projetos/projeto-linux/clean.sh
@@ -8,15 +8,24 @@ fi
 SYS_LOG_DIR="/var/log/nginx/status"
 NGINX_SCRIPT="/usr/bin/nginx_status_check.sh"
 
+# Remove o NGINX e dependências
+sudo apt purge nginx -y
+sudo apt autoremove -y
+clear -x
+
 # Remove o cronjob
 (sudo crontab -l | grep "*/5 * * * * $NGINX_SCRIPT") | sudo crontab -
+echo "Cronjob removido!"
 
 # Remove a pasta de logs
 sudo rm -r "$SYS_LOG_DIR"
+echo "Pasta '$SYS_LOG_DIR' removida!"
 
 # Remove o script
 sudo rm "$NGINX_SCRIPT"
+echo "Script '$NGINX_SCRIPT' apagado!"
 
-# Remove o NGINX e dependências
-sudo apt purge nginx -y
-sudo apt autoremove
+# Finaliza
+echo ""
+echo "Script finalizado! Monitoramento do NGINX foi REMOVIDO"
+echo ""
