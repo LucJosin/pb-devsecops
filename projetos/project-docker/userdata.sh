2a41ebd44130be512007374a4c632726476b646b
Lucas Josino <contact@lucasjosino.com>
2025-03-18 15:35:39 -0300

    refactor: reformula script

--- projetos/project-docker/userdata.sh
+++ projetos/project-docker/userdata.sh
@@ -105,15 +105,15 @@ echo "[userdata.sh] Done! CloudWatch Agent has been configured and started."
 # Instalando Docker Engine utilizando APT
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 
-echo "Starting Docker installation..."
+echo "[userdata.sh] Starting Docker installation..."
 
 ## Adiciona a chave 'GPG' do Docker:
-echo "Setting up Docker APT repository..."
+echo "[userdata.sh] Setting up Docker APT repository..."
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "Done! The Docker repository is now configured."
+echo "[userdata.sh] Done! The Docker repository is now configured."
 
 ## Adiciona o repositório ao APT sources:
 echo \
@@ -126,39 +126,40 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 ## Testando a instalação:
-if ! docker --version > /dev/null 2>&1; then echo "Something went wrong! Docker isn't running."; exit 1; fi
-if ! docker compose version > /dev/null 2>&1; then echo "Something went wrong! Docker compose isn't running."; exit 1; fi
+if ! docker --version > /dev/null 2>&1; then echo "[userdata.sh] Something went wrong! Docker isn't running."; exit 1; fi
+if ! docker compose version > /dev/null 2>&1; then echo "[userdata.sh] Something went wrong! Docker compose isn't running."; exit 1; fi
 
-echo "Done! The Docker is now installed."
+echo "[userdata.sh] Done! The Docker is now installed."
 
 # Configurando a inicialização do Docker e suas permissões (post install):
 # https://docs.docker.com/engine/install/linux-postinstall/
 
 ## Atualiza as permissões:
-echo "Setting up Docker permissions..."
+echo "[userdata.sh] Setting up Docker permissions..."
 sudo groupadd docker 2>/dev/null # ignore if group already exists
 sudo usermod -aG docker $USER
 #newgrp docker # activate the changes to groups
-echo "Done! The Docker permissions is now configured."
+echo "[userdata.sh] Done! The Docker permissions is now configured."
 
 ## Inicia o serviço do Docker:
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
-echo "Enabled Docker and Containerd services!"
-echo "Use 'sudo systemctl status docker.service' to see the current status."
+echo "[userdata.sh] Enabled Docker and Containerd services!"
+echo "[userdata.sh] Use 'sudo systemctl status docker.service' to see the current status."
 
 # Configuração do EFS:
-echo "Setting up EFS, installing 'nfs-common'..."
+echo "[userdata.sh] Setting up EFS, installing 'nfs-common'..."
 sudo apt install nfs-common -y
 sudo mkdir -p /mnt/efs
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <EFS_IP>:/ /mnt/efs
-echo "Done! The EFS is now configured at '/efs'"
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport "$EFS_ENDPOINT":/ /mnt/efs
+echo "[userdata.sh] Done! The EFS is now configured at '/efs'"
 
 # Cria e configura o arquivo 'compose.yaml':
-PROJECT_PATH="/opt/wordpress"
-mkdir -p "$PROJECT_PATH"  # create the directory
+COMPOSE_PATH="/opt/wordpress"
+WORDPRESS_PATH="/mnt/efs/wordpress"
+mkdir -p "$COMPOSE_PATH"  # create the directory
 
-cat << EOF > "$PROJECT_PATH/compose.yaml"
+cat << EOF > "$COMPOSE_PATH/compose.yaml"
 services:
   wordpress:
     image: wordpress
@@ -166,15 +167,48 @@ services:
     ports:
       - "80:80"
     environment:
-      WORDPRESS_DB_HOST: <DB_HOST>
-      WORDPRESS_DB_USER: <DB_USER>
-      WORDPRESS_DB_PASSWORD: <DB_PASSWORD>
-      WORDPRESS_DB_NAME: <DB_NAME>
+      WORDPRESS_DB_HOST: $WORDPRESS_DB_HOST
+      WORDPRESS_DB_USER: $WORDPRESS_DB_USER
+      WORDPRESS_DB_PASSWORD: $WORDPRESS_DB_PASSWORD
+      WORDPRESS_DB_NAME: $WORDPRESS_DB_NAME
     volumes:
-      - "/mnt/efs/wordpress:/var/www/html"
+      - "$WORDPRESS_PATH:/var/www/html"
 EOF
-echo "Compose file created at '$PROJECT_PATH/compose.yaml'!"
+echo "[userdata.sh] Compose file created at '$COMPOSE_PATH/compose.yaml'!"
 
-# Inicia o Docker compose
-cd "$PROJECT_PATH"
-docker compose -p wordpress up -d
+# Cria o arquivo de healthcheck:
+echo "[userdata.sh] Creating healthcheck file..."
+cat << EOF > "$WORDPRESS_PATH/healthcheck.php"
+<?php
+http_response_code(200);
+exit('OK');
+?>
+EOF
+echo "[userdata.sh] Healthcheck file created at '$WORDPRESS_PATH'!"
+
+# Converte o Wordpress em um Service:
+SERVICE_PATH="/etc/systemd/system"
+echo "[userdata.sh] Creating wordpress service file..."
+cat << EOF | sudo tee "$SERVICE_PATH/wordpress.service"
+[Unit]
+Description=WordPress Service
+After=docker.service
+Requires=docker.service
+
+[Service]
+Restart=always
+ExecStart=/usr/bin/docker compose -f $COMPOSE_PATH/compose.yaml up -d
+ 
+[Install]
+WantedBy=multi-user.target
+EOF
+echo "[userdata.sh] Wordpress service file created at '$SERVICE_PATH/wordpress.service'!"
+
+# Configura wordpress.service usando systemctl:
+sudo systemctl enable wordpress.service
+sudo systemctl start wordpress.service
+echo "[userdata.sh] Wordpress service enabled using 'systemctl'!"
+
+# Finaliza
+echo ""
+echo "[userdata.sh] Wordpress has installed with success! Verify the status using 'sudo systemctl status wordpress.service'"
